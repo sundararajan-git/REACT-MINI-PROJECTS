3e004badc7c92f03e3fe12191ad58070a483a7a3
sundararajan-git <sundararajanselvarasu@gmail.com>
2025-09-19 14:40:48 +0530

    up dep

--- BOOK-FINDER-APP/vite.config.js
+++ BOOK-FINDER-APP/vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/BOOK-FINDER-APP/",
   plugins: [react(), tailwindcss()],
+  base: "/BOOK-FINDER-APP/",
 })
