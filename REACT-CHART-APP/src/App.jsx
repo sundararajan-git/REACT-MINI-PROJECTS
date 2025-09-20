6917a59b473a716dfc72e49f5c53d64bb67f00d5
sundararajan-git <sundararajanselvarasu@gmail.com>
2025-09-20 11:54:01 +0530

    up from react chart app

--- REACT-CHART-APP/src/App.jsx
+++ REACT-CHART-APP/src/App.jsx
@@ -17,6 +17,8 @@ ChartJS.register(
   Tooltip,
   Legend
 );
+import "./App.css";
+import { FaChartSimple } from "react-icons/fa6";
 
 export default function App() {
   const [data] = useState({
@@ -25,7 +27,7 @@ export default function App() {
       {
         label: "Sales",
         data: [12, 19, 8, 14, 20, 16],
-        backgroundColor: "rgba(59,130,246,0.8)",
+        backgroundColor: "#9810fa",
       },
     ],
   });
@@ -39,9 +41,12 @@ export default function App() {
   };
 
   return (
-    <div className="min-h-screen p-6 bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100">
-      <h1 className="text-3xl mb-4">📊 React Chart (Chart.js)</h1>
-      <div className="bg-white dark:bg-gray-800 p-4 rounded shadow max-w-3xl">
+    <div className="h-screen w-full flex flex-col items-center justify-center bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100">
+      <div className="flex items-center justify-center gap-3 mb-6 w-full">
+        <FaChartSimple className="text-3xl text-purple-600" />
+        <h1 className="text-3xl font-semibold">React Chart (Chart.js)</h1>
+      </div>
+      <div className="bg-white dark:bg-gray-800 p-4 rounded shadow w-full max-w-[50%]">
         <Bar data={data} options={options} />
       </div>
     </div>
