fa1c72b698dff9cdacb308a60063081f4005dd6f
sundararajan-git <sundararajanselvarasu@gmail.com>
2025-09-20 10:56:29 +0530

    up from music player

--- MUSIC-PLAYER-UI/src/App.jsx
+++ MUSIC-PLAYER-UI/src/App.jsx
@@ -1,19 +1,6 @@
-import React, { useState, useRef } from "react";
-
-const tracks = [
-  {
-    name: "Track 1",
-    src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3",
-  },
-  {
-    name: "Track 2",
-    src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3",
-  },
-  {
-    name: "Track 3",
-    src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3",
-  },
-];
+import { useState, useRef } from "react";
+import "./App.css";
+import { FaHeadphonesAlt } from "react-icons/fa";
 
 export default function App() {
   const [currentTrack, setCurrentTrack] = useState(0);
@@ -41,7 +28,10 @@ export default function App() {
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 p-6">
-      <h1 className="text-4xl mb-6 font-bold">🎧 Music Player</h1>
+      <div className="flex items-center gap-3 justify-center mb-8">
+        <FaHeadphonesAlt className="text-4xl text-purple-600" />
+        <h1 className="text-4xl font-semibold">Music Player</h1>
+      </div>
 
       <p className="text-2xl mb-4">{tracks[currentTrack].name}</p>
 
@@ -54,19 +44,19 @@ export default function App() {
       <div className="flex gap-4 mb-4">
         <button
           onClick={prevTrack}
-          className="px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+          className="px-6 py-2 bg-purple-500 text-white rounded hover:bg-purple-600 cursor-pointer"
         >
           Prev
         </button>
         <button
           onClick={togglePlay}
-          className="px-6 py-2 bg-green-500 text-white rounded hover:bg-green-600"
+          className="px-6 py-2 bg-green-500 text-white rounded hover:bg-green-600 cursor-pointer"
         >
           {isPlaying ? "Pause" : "Play"}
         </button>
         <button
           onClick={nextTrack}
-          className="px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+          className="px-6 py-2 bg-purple-500 text-white rounded hover:bg-purple-600 cursor-pointer"
         >
           Next
         </button>
@@ -78,3 +68,18 @@ export default function App() {
     </div>
   );
 }
+
+const tracks = [
+  {
+    name: "Track 1",
+    src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3",
+  },
+  {
+    name: "Track 2",
+    src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3",
+  },
+  {
+    name: "Track 3",
+    src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3",
+  },
+];
