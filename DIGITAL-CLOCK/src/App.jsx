29b751a9ccd4d3abcb42fa13d76877293f62c198
sundararajan-git <sundararajanselvarasu@gmail.com>
2025-09-20 09:24:04 +0530

    up from digital clock

--- DIGITAL-CLOCK/src/App.jsx
+++ DIGITAL-CLOCK/src/App.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react";
+import "./App.css";
+import { GoClockFill } from "react-icons/go";
 
 const App = () => {
   const [time, setTime] = useState(new Date());
@@ -12,8 +14,25 @@ const App = () => {
   }, []);
 
   return (
-    <div className="container">
-      <h1>{time.toLocaleTimeString()}</h1>
+    <div className="min-h-screen flex flex-col items-center justify-center gap-4 bg-gray-100">
+      <div className="flex items-center gap-3 mb-4">
+        <GoClockFill className="text-2xl text-gray-900" />
+        <h2 className="text-purple-600 font-semibold text-2xl">
+          Digital Clock
+        </h2>
+      </div>
+      <div className="bg-white/10 backdrop-blur-lg p-10 rounded-2xl  text-center">
+        <h1 className="text-6xl font-mono tracking-widest drop-shadow-lg">
+          {time
+            .toLocaleTimeString([], {
+              hour: "2-digit",
+              minute: "2-digit",
+              second: "2-digit",
+              hour12: true,
+            })
+            .toUpperCase()}
+        </h1>
+      </div>
     </div>
   );
 };
