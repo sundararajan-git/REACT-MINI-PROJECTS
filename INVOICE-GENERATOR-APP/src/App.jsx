fd0bee69d86096d2ccbbf11ef12bad663035aa91
sundararajan-git <sundararajanselvarasu@gmail.com>
2025-09-20 10:14:19 +0530

    up from invoice generator

--- INVOICE-GENERATOR-APP/src/App.jsx
+++ INVOICE-GENERATOR-APP/src/App.jsx
@@ -1,4 +1,7 @@
-import React, { useState } from "react";
+import { useState } from "react";
+import "./App.css";
+import { PiInvoice } from "react-icons/pi";
+import { IoClose } from "react-icons/io5";
 
 export default function App() {
   const [items, setItems] = useState([{ desc: "", qty: 1, price: 0 }]);
@@ -14,13 +17,20 @@ export default function App() {
 
   const total = items.reduce((sum, i) => sum + i.qty * i.price, 0);
 
+  const genrateHandler = () => {
+    alert("Invoice Generated !");
+  };
+
   return (
     <div className="min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 p-6 flex flex-col items-center">
-      <h1 className="text-4xl mb-6 font-bold">🧾 Invoice Generator</h1>
+      <div className="flex items-center gap-3 mb-6">
+        <PiInvoice className="text-3xl text-purple-600" />
+        <h1 className="text-4xl font-semibold">Invoice Generator</h1>
+      </div>
 
-      <div className="w-full max-w-3xl">
+      <div className="w-full max-w-3xl flex flex-col gap-4">
         {items.map((item, index) => (
-          <div key={index} className="flex gap-2 mb-2">
+          <div key={index} className="flex gap-2 mb-2 items-center">
             <input
               type="text"
               placeholder="Description"
@@ -44,19 +54,27 @@ export default function App() {
             />
             <button
               onClick={() => removeItem(index)}
-              className="px-3 bg-red-500 text-white rounded hover:bg-red-600"
+              className="p-1.5 bg-red-500 rounded-full text-white hover:bg-red-600 cursor-pointer"
             >
-              X
+              <IoClose className="text-lg" />
             </button>
           </div>
         ))}
-        <button
-          onClick={addItem}
-          className="px-6 py-2 bg-green-500 text-white rounded hover:bg-green-600 mb-4"
-        >
-          Add Item
-        </button>
-        <p className="text-xl font-bold">Total: ${total.toFixed(2)}</p>
+        <div className="flex items-center gap-4">
+          <button
+            onClick={addItem}
+            className="px-6 py-2 bg-green-500 text-white rounded hover:bg-green-600 mb-4 cursor-pointer"
+          >
+            Add Item
+          </button>
+          <button
+            className="px-6 py-2 bg-purple-500 text-white rounded hover:bg-purple-800 mb-4 cursor-pointer"
+            onClick={genrateHandler}
+          >
+            Generate
+          </button>
+        </div>
+        <p className="text-xl font-medium">Total: ${total.toFixed(2)}</p>
       </div>
     </div>
   );
