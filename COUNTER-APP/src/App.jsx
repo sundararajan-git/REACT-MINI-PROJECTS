c7e11029e7cdbee50c25f5454d5a96a5d252ab77
sundararajan-git <sundararajanselvarasu@gmail.com>
2025-09-20 07:54:20 +0530

    up from counter app

--- COUNTER-APP/src/App.jsx
+++ COUNTER-APP/src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import "./App.css";
+import { SiCountingworkspro } from "react-icons/si";
 
 const App = () => {
   const [count, setCount] = useState(0);
@@ -9,7 +11,10 @@ const App = () => {
 
   return (
     <div className="flex flex-col gap-4 justify-center items-center  w-full h-screen">
-      <h1 className="font-semibold text-xl">Counter App</h1>
+      <div className="flex items-center gap-3">
+        <SiCountingworkspro className="text-2xl" />
+        <h1 className="font-semibold text-2xl text-purple-600">Counter</h1>
+      </div>
       <br />
       <div className="flex flex-col gap-4">
         <p className="font-bold text-3xl">{count}</p>
@@ -17,7 +22,7 @@ const App = () => {
       <br />
       <button
         type="button"
-        className="px-4 py-2 rounded-md text-white hover:cursor-pointer bg-green-600"
+        className="px-4 py-2 rounded-md text-white hover:cursor-pointer bg-purple-600"
         onClick={countHandler}
       >
         Count
